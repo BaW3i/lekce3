13c5b7f9d7ac3f597744cc61bca8ab74c978e704
Barbora Weigertova <barbora.weigertova@previo.cz>
2024-10-24 20:05:34 +0200

    update

---
+++ lekce3.js
@@ -0,0 +1,35 @@
+console.log('bbeeep')
+
+
+let getName = prompt("Please enter your name");
+let text;
+if (person == null || person == "") {
+  text = "User cancelled the prompt.";
+} else {
+  text = "Hello " + person + "! How are you today?";
+}
+
+let getAge = prompt("Please enter your age");
+let age;
+if (year == null || year == "") {
+  text = "This is not an age.";
+} else {
+  text = "Oh, you are " + year + "!";
+}
+
+console.log(text, year)
+
+
+/* Představte si, že vyrábíte registrační systém na očkování proti COVID-19. U každého registrovaného chceme evidovat jméno a věk.
+
+Vytvořte webovou stránku, která se uživatele zeptá nejdříve na jméno a poté na věk. Tyto hodnoty si uložte do smysluplně pojmenovaných proměnných. Nezpomeňte věk převést na číslo.
+Poté, co uživatel zadá všechny údaje, vypište do stránky odstavec s obsahem ve tvaru
+Květoslav Voňavý, věk: 67 */
+
+
+/* Vytvořte webovou stránku, která uživatele požádá o jeho hodinovou sazbu v korunách a spočítá jeho hrubou mzdu za předpokladu, že pracuje 8 hodin denně 21 dní v měsíci. Do stránky vypište výsledek zabalený do nějaké přívětivé formulace. Dbejte na to, abyste korektně převedli uživatelem zadanou hodnotu na číslo.
+Nechte uživatele zadat nejen hodinovou sazbu, ale také počet hodin a dní v měsíci. Opět dejte pozor na správnou konverzi. */
+
+
+
+/* dodělej ta cvičení, co byla za úkol, cses testovals */
