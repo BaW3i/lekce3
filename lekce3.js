aeca523c19209d30d82cfec0eee8507cf67b813c
Barbora Weigertova <barbora.weigertova@previo.cz>
2024-10-24 21:17:40 +0200

    hehe

--- lekce3.js
+++ lekce3.js
@@ -1,35 +1,64 @@
-console.log('bbeeep')
+/* console.log('bbeeep')
+const hodinovka = 300;
+const pocetHodin = Number(prompt("Zadej hodiny", "160")); //fce number makes from string num
+const plat = hodinovka * pocetHodin;
 
-
-let getName = prompt("Please enter your name");
-let text;
-if (person == null || person == "") {
-  text = "User cancelled the prompt.";
-} else {
-  text = "Hello " + person + "! How are you today?";
-}
-
-let getAge = prompt("Please enter your age");
-let age;
-if (year == null || year == "") {
-  text = "This is not an age.";
-} else {
-  text = "Oh, you are " + year + "!";
-}
-
-console.log(text, year)
-
-
-/* Představte si, že vyrábíte registrační systém na očkování proti COVID-19. U každého registrovaného chceme evidovat jméno a věk.
-
-Vytvořte webovou stránku, která se uživatele zeptá nejdříve na jméno a poté na věk. Tyto hodnoty si uložte do smysluplně pojmenovaných proměnných. Nezpomeňte věk převést na číslo.
-Poté, co uživatel zadá všechny údaje, vypište do stránky odstavec s obsahem ve tvaru
-Květoslav Voňavý, věk: 67 */
-
-
-/* Vytvořte webovou stránku, která uživatele požádá o jeho hodinovou sazbu v korunách a spočítá jeho hrubou mzdu za předpokladu, že pracuje 8 hodin denně 21 dní v měsíci. Do stránky vypište výsledek zabalený do nějaké přívětivé formulace. Dbejte na to, abyste korektně převedli uživatelem zadanou hodnotu na číslo.
-Nechte uživatele zadat nejen hodinovou sazbu, ale také počet hodin a dní v měsíci. Opět dejte pozor na správnou konverzi. */
+document.body.innerHTML = "<p> Pepa si vydělal" + plat + " korun </p>"
 
 
 
-/* dodělej ta cvičení, co byla za úkol, cses testovals */
+const jméno = prompt("Zadej jméno", "vaněk");
+const věk = Number(prompt("Zadej věk", "48")); //fce number makes from string num
+
+document.body.innerHTML = "<p> bumbum" + věk + jméno + " bum </p>" 
+
+
+const hodinovýPlat = Number(prompt("zadej hodinovku", "0"));
+const měsíčně = 8*21*hodinovýPlat; 
+
+document.body.innerHTML = "<p> bude mít" + měsíčně + " bum </p>" */
+
+
+//realitka
+
+/* Vytvořte webovou stránku s JavaScriptem, zkopírujte si tento kód do vašeho programu a vyřešte následující úkoly.
+
+Pomocí tečkové notace vypište do stránky dispozici bytu. YES
+Vypište do stránky čistý nájem bez poplatků.  YES
+Vypište do stránky celý objekt představující výměru bytu. YES
+Do separátních proměnných uložte město a městskou část. Vypište je do stránky. 
+Změnte stav inzerátu z 'free' na 'taken'. */
+
+const apartment = {
+  type: 'rent',
+  disposition: '3+1',
+  area: {
+    floorage: 100,
+    balcony: 2,
+    units: 'sqm',
+  },
+  city: 'Prague',
+  district: 'Old Town',
+  price: {
+    rent: 28000,
+    fees: {
+      water: 1000,
+      energy: 2500,
+      services: 560,
+    },
+    currency: 'czk',
+  },
+  ownership: 'personal',
+  condition: 'renovated',
+  status: 'free',
+  floor: 3,
+};
+
+document.body.innerHTML += "<p> dispozice je" + apartment.disposition + " bum </p>"
+document.body.innerHTML += "<p> prachy jsou" + apartment.price.rent + " bum </p>"
+document.body.innerHTML += "<p>Vymera bytu: "+ apartment.area.floorage + apartment.area.units +"</p>"
+
+const city = apartment.city
+const district = apartment.district
+apartment.status = "taken";
+document.body.innerHTML += "<p>Stav inzeratu: "+ apartment.status +"</p>"
