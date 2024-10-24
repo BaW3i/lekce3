6565576c6aff871755cbc224a6b3ba5d18a1aa1b
Barbora Weigertova <barbora.weigertova@previo.cz>
2024-10-24 21:32:24 +0200

    bum

--- lekce3.js
+++ lekce3.js
@@ -62,3 +62,33 @@ const city = apartment.city
 const district = apartment.district
 apartment.status = "taken";
 document.body.innerHTML += "<p>Stav inzeratu: "+ apartment.status +"</p>"
+
+
+/* u absolute asshat lil'ass jellyass mushbrained retard ur commiting commands look as such
+
+PS C:\Users\matya\Desktop\code czechitas\lekce3> git add .
+PS C:\Users\matya\Desktop\code czechitas\lekce3> git push
+Everything up-to-date
+PS C:\Users\matya\Desktop\code czechitas\lekce3> git -m "message" */
+
+// COVID OBJEKT 
+
+/* Vytvořte objekt person, do kterého vložte uživatelem zadané údaje. Objekt bude mít následující strukturu
+{
+  name: 'Květoslav Voňavý',
+  age: 67,
+}
+Přidejte do objektu person údaj o tom, v jakém jazyce si uživatel přeje komunikovat. Zjistěte jej z objektu window.
+Poté, co uživatel zadá všechny údaje, vypište do stránky objekt person se všemi jeho vlastnostmi v nějakém hezkém formátu a zkontrolujte, 
+že obsahuje správné informace.
+*/
+
+const person = {
+  name: prompt("Jaké je vaše jméno a příjmení?"),
+  age: Number(prompt("Jaký je váš věk?")),
+  language: window.navigator.language,
+};
+
+document.body.innerHTML += "<p>Zadané jméno: " + person.name + ", váš věk: " + person.age + "</p>";
+document.body.innerHTML += "<p>Váš jazyk: " + person.language + "</p>";
+document.body.innerHTML += "<p>Byli jste zaregistrováni na očkování.</p>";
